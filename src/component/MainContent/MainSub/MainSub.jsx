e1a4ce168ed49338ff7027e0dcb05f939f627977
Moulishwaran <moulish0809@gmail.com>
2024-03-19 15:58:05 +0530

    CommiChange(19-03-2024)

--- src/component/MainContent/MainSub/MainSub.jsx
+++ src/component/MainContent/MainSub/MainSub.jsx
@@ -7,55 +7,51 @@ const MainSub = () => {
       <h1 className="MainSub-MainTitle">
         <span className="MainSub-Why">Why</span> Autointelli?
       </h1>
-      <div className="line"></div>
-      <div className="content">
-        <h1 className="MainSub-RealTitle">
-          Real, actionable insights into your stack
-        </h1>
-        <p className="MainSub-Correlate">
-          Correlate issues across your stack. Debug and collaborate from your
-          <br />
-          IDE. AI assistance at every step. All in one connected experience -
-          not <br /> a maze of charts
-        </p>
-      </div>
-      <div className="lines"></div>
+      <div className="MainSub-ContentWrapperLeft">
+        <div className="line"></div>
+        <div className="content">
+          <h1 className="MainSub-RealTitle">
+            Real, actionable insights into your stack
+          </h1>
+          <p className="MainSub-Correlate">
+            Correlate issues across your stack. Debug and collaborate from your
+            IDE.
+          </p>
+        </div>
+        <div className="lines"></div>
 
-      <div className="MainSub-content1">
-        <h1 className="MainSub-RealTitle1">
-          Dashboards, alerts, and integrations all in one place
-        </h1>
-        <p className="MainSub-Correlate1">
-          Our Instant Observability quickstarts bundle everything you need to
-          <br />
-          start monitoring like a pro right out of the box. <br />
-        </p>
+        <div className="MainSub-content1">
+          <h1 className="MainSub-RealTitle1">
+            Dashboards, alerts, and integrations all in one place
+          </h1>
+          <p className="MainSub-Correlate1">
+            Our Instant Observability quickstarts bundle everything you need.
+          </p>
+        </div>
       </div>
 
-      <div className="orangeLine"></div>
-      <div className="MainSub-contents">
-        <h1 className="MainSub-RealTitles">
-          Troubleshoot infrastructure before. Not after.
-        </h1>
-        <p className="MainSub-Correlates">
-          Find root cause faster. Correlate infrastructure health with
-          <br />
-          performance and customer impact on one platform.
-          <br />
-        </p>
-      </div>
-      <div className="orangeLines"></div>
+      <div className="MainSub-ContentWrapperRight">
+        <div className="orangeLine"></div>
+        <div className="MainSub-contents">
+          <h1 className="MainSub-RealTitles">
+            Troubleshoot infrastructure before. Not after.
+          </h1>
+          <p className="MainSub-Correlates">
+            Find root cause faster. Correlate infrastructure health with
+            performance and customer
+          </p>
+        </div>
+        <div className="orangeLines"></div>
 
-      <div className="MainSub-contents2">
-        <h1 className="MainSub-RealTitles2">
-          Troubleshoot infrastructure before. Not after.
-        </h1>
-        <p className="MainSub-Correlates2">
-          Integrate easily with leading cloud providers. New Relic is committed
-          <br />
-          to open standards, open instrumentation, and the open communities
-          <br />
-        </p>
+        <div className="MainSub-contents2">
+          <h1 className="MainSub-RealTitles2">
+            Troubleshoot infrastructure before. Not after.
+          </h1>
+          <p className="MainSub-Correlates2">
+            Integrate easily with leading cloud providers. New Relic is
+            committed
+          </p>
+        </div>
       </div>
     </div>
   );
